c75e53899edf3aa8f1312981175ad1b168538c24
Genevieve <genevieve@Genevieves-MacBook-Pro.local>
2022-02-23 11:54:50 -0800

    added to the for loop

--- R/kit_fox_resistance.R
+++ R/kit_fox_resistance.R
@@ -1,11 +1,12 @@
 #' Kit Fox Resistance Across a Landscape
 #' 
-#' This function determines automobile power generation
-#' @param r resistance coefficient (100)
-#' @param l resistance coefficient 2 (99)
-#' @param c kit fox sensitivity to habitat suitability score
-#' @param h habitat suitability score
-
+#' This function determines kit fox resistance across a landscape given habitat suitability and sensitivity
+#' @param r resistance coefficient (default = 100)
+#' @param l resistance coefficient 2 (default = 99)
+#' @param c kit fox sensitivity to habitat suitability score 
+#' @param h habitat suitability score 
+#' @return resistance 
+#' 
 #function definition
 kit_fox_resistance = function(r=100, l=99, c, h) {
   result = r - l * ((1 - exp(-c * h)) / (1 - exp(-c)))
