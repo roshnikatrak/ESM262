1e59978bc14a5301b39ef274911ca2bbdbb7f8c0
Genevieve <genevieve@Genevieves-MacBook-Pro.local>
2022-02-24 09:12:56 -0800

    added pmap analysis and fixed color palettes

--- R/kit_fox_resistance.R
+++ R/kit_fox_resistance.R
@@ -9,6 +9,13 @@
 #' 
 #function definition
 kit_fox_resistance = function(r=100, m=99, c, h) {
+  # make sure that habitat suitability inputs are between 0 and 1 
+  h = ifelse(h >= 0, ifelse(h <=1, h, NA), NA)
+  #h = ifelse((h < 0 & h > 1), NA, h) 
+  #if (h > 1) return ("Habitat suitability score cannot be greater than 1")
+  #if (h < 0) return("Habitat suitability score cannot be less than 0")
+  # make sure that sensitivity inputs are greater than 0 
+  c = ifelse((c < 0), return ("Sensitivity must be greater than 0"), c)
   result = r-m*((1-exp(-c*h))/(1-exp(-c)))
   return(result)
 }
