97acba606a7447dd053360bfad85686daaffcb87
roshnikatrak <rkatrak-adefowora@ucsb.edu>
2022-02-27 10:28:53 -0800

    edited error checking

--- R/kit_fox_resistance.R
+++ R/kit_fox_resistance.R
@@ -3,19 +3,18 @@
 #' This function determines kit fox resistance across a landscape given habitat suitability and sensitivity
 #' @param r resistance coefficient (default = 100)
 #' @param m resistance coefficient 2 (default = 99)
-#' @param c kit fox sensitivity to habitat suitability score 
-#' @param h habitat suitability score 
+#' @param c kit fox sensitivity to habitat suitability score. Must be greater than or equal to 0.
+#' @param h habitat suitability score. Must be greater than or equal to 0 and less than or equal to 1.
 #' @return resistance (ohm)
 #' 
 #function definition
 kit_fox_resistance = function(r=100, m=99, c, h) {
-  # make sure that habitat suitability inputs are between 0 and 1 
+  # make sure that habitat suitability inputs are between 0 and 1. If not, return NA.
   h = ifelse(h >= 0, ifelse(h <=1, h, NA), NA)
-  #h = ifelse((h < 0 & h > 1), NA, h) 
-  #if (h > 1) return ("Habitat suitability score cannot be greater than 1")
-  #if (h < 0) return("Habitat suitability score cannot be less than 0")
-  # make sure that sensitivity inputs are greater than 0 
-  c = ifelse((c < 0), return ("Sensitivity must be greater than 0"), c)
+  
+  # make sure that sensitivity inputs are greater than 0. If not, return NA.
+  c = ifelse(c > 0, c, NA)
+  
   result = r-m*((1-exp(-c*h))/(1-exp(-c)))
   return(result)
 }
